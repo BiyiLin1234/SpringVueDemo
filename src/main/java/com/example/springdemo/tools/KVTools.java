d2fe65a1d28507ac7e0a1b285ac25ef8356ef451
biyilin <2212526859@qq.com>
2022-09-14 20:09:56 +0800

    -- add TOEFL tools and file store tools.

---
+++ src/main/java/com/example/springdemo/tools/KVTools.java
@@ -0,0 +1,69 @@
+package com.example.springdemo.tools;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KVTools {
+    private static final String kvPath = "src/main/resources/notes/store_map.txt";
+    private static KVTools kvTools;
+    private static final String splitter = "\\|";
+
+    private HashMap<String, String> map = new HashMap<>();
+
+    private KVTools() throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(
+                                kvPath
+                        )
+                )
+        );
+
+        String s;
+        while ((s = br.readLine()) != null) {
+            String[] arr = s.split(splitter);
+            map.put(arr[0], arr[1]);
+        }
+        br.close();
+
+    }
+
+    public static KVTools getKVInstance() {
+        try {
+            if (kvTools == null) {
+                synchronized (KVTools.class) {
+                    if (kvTools == null) {
+                        kvTools = new KVTools();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return kvTools;
+    }
+
+    public String getStrKeyStrValue(String key) {
+        return map.getOrDefault(key, "");
+    }
+
+    public void putStrKeyStrValue(String key, String value) {
+        map.put(key, value);
+        save();
+    }
+
+    private void save() {
+        try {
+            BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(kvPath, false)
+            );
+            for (Map.Entry<String, String> e : map.entrySet()) {
+                bw.write(e.getKey() + '|' + e.getValue() + "\n");
+            }
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/springdemo/tools/TOEFLTool.java
+++ src/main/java/com/example/springdemo/tools/TOEFLTool.java
@@ -6,8 +6,12 @@ import java.util.*;
 public class TOEFLTool {
     private static Random random = new Random(System.currentTimeMillis());
     private static Scanner scanner = new Scanner(System.in);
+    private static final String TOEFL_KEY = "TOEFL";
+    private static final String STOP_CMD = "stop";
+    private static final String ADD_CMD = "a"; // 添加到生词表
 
     private static final String path = "src/main/resources/notes/toefl.txt";
+    private static final String path_me = "src/main/resources/notes/toefl_me.txt";
 
     public static void main(String[] args) throws IOException {
         File file = new File(path);
@@ -18,7 +22,7 @@ public class TOEFLTool {
         while (line != null) {
             StringBuilder word = new StringBuilder();
             int idx = 0;
-            while (idx < line.length() && line.charAt(idx) != ' ') {
+            while (idx < line.length() && line.charAt(idx) != '\t') {
                 word.append(line.charAt(idx));
                 idx++;
             }
@@ -28,12 +32,41 @@ public class TOEFLTool {
             });
             line = br.readLine();
         }
-        while (true) {
-            int next = random.nextInt(words.size());
-            System.out.println(words.get(next)[0]);
-            scanner.nextLine();
-            System.out.println(words.get(next)[1]);
-            scanner.nextLine();
+
+        int idx = Integer.parseInt(KVTools.getKVInstance().getStrKeyStrValue(TOEFL_KEY));
+        idx++;
+        System.out.println("本次开始单词索引"  + idx);
+        while (idx < words.size()) {
+            System.out.println("==================================");
+            printWordAndDescribe(words.get(idx));
+            String next = scanner.nextLine();
+            if (next.equals(STOP_CMD)) {
+                KVTools.getKVInstance().putStrKeyStrValue(TOEFL_KEY, String.valueOf(idx));
+                return;
+            } else if (next.equals(ADD_CMD)) {
+                saveMyWord(words.get(idx));
+            }
+            idx++;
+            System.out.println("===================================");
+        }
+    }
+
+    private static void printWordAndDescribe(String[] arr) {
+        System.out.println(arr[0]);
+        scanner.nextLine();
+        System.out.println(arr[1]);
+    }
+
+    private static void saveMyWord(String[] arr) {
+        try {
+            BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(path_me, true)
+            );
+            bw.append(arr[0]).append(" ").append(arr[1]).append("\n");
+            bw.close();
+            System.out.println("added!");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
